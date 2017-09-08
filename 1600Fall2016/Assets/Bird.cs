98fec7ae53a1f4cb4e99dc0aabe07ad3f2b5d43a
Unknown <mitchellwagner1997@gmail.com>
2017-09-08 08:28:21 -0600

    Added Convention and Syntax comments to bird

--- 1600Fall2016/Assets/Bird.cs
+++ 1600Fall2016/Assets/Bird.cs
@@ -1,13 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//Convention is the organization of code to make things easier to find, such as comments,
+//and code blocks. These comments would be an example of Convention. 
 
 public class Bird : Animal {
+	//This code block would also be an example of convention. It starts here,
 public override void Start(){
 	base.Start();
 	Fly();
+	//And ends here, showing us one task we're asking a program to do.
 }
+//Syntax is the organization of our statements so that the computer can read it.
+//For example, the parenthesis below allow us to add a modifier to the command void Fly
 void Fly (){
-	print(this.name + " Flies");
+	//Another example, the curly braces show the start and end of this data block.
+	print(this.name + " Flies");//This semicolon shows the end of the statement.
+	//The double quotation marks shows our string of text
 }
 }
