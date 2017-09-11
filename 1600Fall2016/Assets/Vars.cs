3ca7728b648e1361735a8c6334af1526086a6e9e
Unknown <mitchellwagner1997@gmail.com>
2017-09-11 10:22:32 -0600

    Added Variable Assignment Comments

--- 1600Fall2016/Assets/Vars.cs
+++ 1600Fall2016/Assets/Vars.cs
@@ -2,21 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*Variables are places for storing data,
+ so called because the info in them can change when a project is running.*/
 public class Vars : MonoBehaviour {
 
 	public float health = 100;
-
-	// Use this for initialization
+/*This would be considered a variable as our health can change 
+based on the other code blocks in the program adding or subtracting from it.*/
 	void Start () {
 		health = 200;
 	}
-
+//As you can see the above value was changed by this variable function
 	void OnTriggerEnter()
 	{
 		health += 20;
 	}
-	
-	// Update is called once per frame
+//The variable void keeps our functions from sending back any values back to us
+//Variables can be named for convenience and are usually preceeded by a data type.
+/*For example, int YourAge, int is the data type signifying we need a number input
+while YourAge is the variable that identifies this specific data statement*/
 	void Update () {
 		health = 200;
 	// 	if (health > 0)
