55e5887a9fe1ebb1e5912dc8ac32d6c3b9528753
Unknown <mitchellwagner1997@gmail.com>
2017-09-11 11:55:14 -0600

    Added If Statements

---
+++ 1600Fall2016/Assets/IfStatements.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IfStatements : MonoBehaviour {
+	public Text input;
+	public string password = "tacos";
+
+	void Update (){
+		if(input.text == password){
+			print("You know the password!?!?");
+		}
+	}
+}
