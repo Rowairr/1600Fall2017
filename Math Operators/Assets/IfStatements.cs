0bf2fcd05b632556f0ce90a5c339c86a34d6d432
Unknown <mitchellwagner1997@gmail.com>
2017-09-11 21:46:39 -0600

    Started If Statements

---
+++ Math Operators/Assets/IfStatements.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfStatements : MonoBehaviour {
+//If statements are used to plug in functions under certain circumstances.
+	void Start () {
+		int x = 7;
+		if (x==7)
+		{
+			print("We've got us a lucky 7");
+		}
+		if(x>7)
+		{
+			print("That's too high");
+		}
+		if(x<7)
+		{
+			print("That's too low");
+		}
+	}
+}
