a4a81889578fffbba91abc09b3c7e23f58afcf76
Unknown <mitchellwagner1997@gmail.com>
2017-09-13 11:47:37 -0600

    Math Operators and If Statements

--- 1600Fall2016/Assets/IfStatements.cs
+++ 1600Fall2016/Assets/IfStatements.cs
@@ -5,11 +5,22 @@ using UnityEngine.UI;
 
 public class IfStatements : MonoBehaviour {
 	public Text input;
+
+	public bool canPlayGame = false;
 	public string password = "tacos";
 
 	void Update (){
 		if(input.text == password){
 			print("You know the password!?!?");
+			canPlayGame = true;
+		} else{
+			print("Username/Password incorrect");
+		}
+
+		if(canPlayGame) {
+			print("Playing Game");
+		}	else {
+			print("Can't Play Yet, Enter a Correct Password");
 		}
 	}
 }
