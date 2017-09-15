1ea83dea954f16b8920a7728ffc69235a94e60b8
Unknown <mitchellwagner1997@gmail.com>
2017-09-15 08:19:16 -0600

    Added Else Clauses

--- Math Operators/Assets/IfStatements.cs
+++ Math Operators/Assets/IfStatements.cs
@@ -12,11 +12,17 @@ public class IfStatements : MonoBehaviour {
 		if (x==7)
 		{
 			print("We've got us a lucky 7");
-		}
+		}else
+			{
+				print("I wanna see a seven");
+			}
 		if(x>7)
 		{
 			print("That's too high");
-		}
+		}else
+			{
+				x -= Time.deltaTime - 0.01f;
+			}
 		if(x<7)
 		{
 			print("That's too low");
@@ -24,14 +30,20 @@ public class IfStatements : MonoBehaviour {
 		if(x<=0)
 		{
 			print("Well, that's that. No more luck.");
+		}else{
+			print("There's still hope!");
 		}
 		if(x<=6)
 		{
 			print("Wait... What's going on?");
+		}else{
+			print("Help!");
 		}
 		if(x<=5)
 		{
 			print("Hey! Stop that!");
+		}else{
+			print("No!!!");
 		}
 		if(x<=4)
 		{
