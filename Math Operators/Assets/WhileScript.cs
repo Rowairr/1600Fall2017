31c078a1484340701bd7e878ea36fb8b11186370
Unknown <mitchellwagner1997@gmail.com>
2017-09-18 11:06:14 -0600

    Started While Statements

---
+++ Math Operators/Assets/WhileScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhileScript : MonoBehaviour {
+public double num = 10;
+public float decrease = 0.3f;
+public double x = 7;
+public double y = 12;
+	void Update () {
+		while(num >= 9)
+		{
+			num -= 0.000001f;
+		}
+	}
+}
