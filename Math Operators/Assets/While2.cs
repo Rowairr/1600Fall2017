b25085a7eff9ec4e2c3284b12df98a6b15c6626e
Unknown <mitchellwagner1997@gmail.com>
2017-09-20 15:43:09 -0600

    Added While Loop Assignment (While2)

---
+++ Math Operators/Assets/While2.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class While2 : MonoBehaviour {
+public double Num1 = 7;
+	void Update () {
+		while (Num1==7)
+		{
+			Num1 -= 1;
+		}
+		while (Num1==6)
+		{
+			Num1 -= 1;
+		}
+		while (Num1==5)
+		{
+			Num1 -= 1;
+		}
+		while (Num1<=4)
+		{
+			Num1 -= 0.5f;
+		}
+		while (Num1 <= 0)
+		{
+			Num1 = 0;
+		}
+	}
+}
