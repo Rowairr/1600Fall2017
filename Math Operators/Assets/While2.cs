d46717b09b0f505cc568d86f9d2e1d6d94c5b8ae
Unknown <mitchellwagner1997@gmail.com>
2017-09-22 08:26:17 -0600

    Completed While Loop assignment

--- Math Operators/Assets/While2.cs
+++ Math Operators/Assets/While2.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class While2 : MonoBehaviour {
 public double Num1 = 7;
+public double Num2 = 4;
 	void Update () {
+		/* A while loop is like an if statement that will continue to run as long
+		as the given input is true */
 		while (Num1==7)
 		{
 			Num1 -= 1;
 		}
+		/*For example, this above function will run only while Num1 is exactly equal
+		to seven, causing it to only run once.*/
 		while (Num1==6)
 		{
 			Num1 -= 1;
@@ -24,6 +29,36 @@ public double Num1 = 7;
 		while (Num1 <= 0)
 		{
 			Num1 = 0;
+		}
+		while (Num1 == 0)
+		{
+			print("While Loop Complete");
+		}
+		while (Num2 == 4)
+		{
+			print("Num2 is currently 4")
+		}
+		while (Num2 <= 4)
+		{
+			if(Num2 >= 3)
+			{
+				Num2 -= 0.5f;
+			}
+				}
+		while (Num2 <= 3)
+		{
+			if(Num2 >= 2)
+			{
+				Num2 -= 0.5f;
+			}
+			else
+			{
+				print("While Loop complete");
+			}
+		while (Num2 <= 2)
+		{
+			print("Final While Loop complete");
+		}
 		}
 	}
 }
