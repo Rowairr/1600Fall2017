547243d3dc451ccbfed463d0d8a0298ba91eb7a7
Unknown <mitchellwagner1997@gmail.com>
2017-10-04 11:04:18 -0600

    Finished Scope Access Assignment

--- Chess Powerup/Assets/PawnPowerup.cs
+++ Chess Powerup/Assets/PawnPowerup.cs
@@ -3,16 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PawnPowerup : MonoBehaviour {
-<<<<<<< Updated upstream
 	void OnTriggerEnter()
     {
         gameObject.SetActive(false);
     }
-=======
-
-void OnTriggerEnter()
-{
-	gameObject.SetActive(false);
-}
->>>>>>> Stashed changes
 }
--- Math Operators/Assets/ScopeAccess.cs
+++ Math Operators/Assets/ScopeAccess.cs
@@ -13,6 +13,23 @@ private float y = 12;
 /*The private access modifier means that this can only be modified by lines
 of code within our c# script and cannot be seen in other programs. All lines
 of code default to private unless stated otherwise. */
+public int r = 5;
+// This int can be affected by other scripts//
+private int q = 4;
+// This int can only be seen by in this script.//
+internal int m = 7;
+// This int can only be affected by this script//
+protected int p = 12;
+/* This int can only be affected by classes derived from this class. Such as if 
+this script were in the Animal class we did earlier, it would only be able to be
+affected by the breakoffs (such as whale, human, and fish)*/
+/* I don't really have anything else to add, so I'll just finish with three
+more examples*/
+public string king = "Royal";
+public float kk = 0.02f;
+private string d = "Done";
+
+
 
 
 }
