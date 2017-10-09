b360244075ca6a0288493d9f268072f84c93822b
Unknown <mitchellwagner1997@gmail.com>
2017-10-09 11:44:20 -0600

    Started Functions with Parameters Assignment

---
+++ 1600Fall2016/Assets/MathFunctions.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MathFunctions : MonoBehaviour {
+public int myScore;
+public int health;
+public int ammo;
+public string myPassword = "CoolStuff";
+public string myDoorPassword;
+	void Start () {
+		myScore = ReturnNum(health, ammo);
+		myPassword = ReturnPassword(myPassword);
+		myDoorPassword = DoorPassword(myDoorPassword);
+	}
+	int ReturnNum(int _num, int _num2) {
+		return _num + _num2;
+	}
+	string ReturnPassword (string _login){
+		if(_login == "ou812"){
+		return "Password Correct";
+	} else{
+		return "Incorrect";
+	}
+	string DoorPassword(string password){
+		if(password == "0825"){
+			return "The door has opened";
+		}else{
+			return "Your fate is still sealed";
+		}
+	}
+}
+}
