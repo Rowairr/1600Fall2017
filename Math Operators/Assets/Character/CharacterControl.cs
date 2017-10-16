ab9fb196dedc5b154ab80a93c67bc73817da15b9
Unknown <mitchellwagner1997@gmail.com>
2017-10-16 11:59:29 -0600

    Added Character Jump

---
+++ Math Operators/Assets/Character/CharacterControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControl : MonoBehaviour {
+public CharacterController charactercontroller;
+public float gravity = 9.81f;
+public float speed = 12;
+public Vector3 moveVector3;
+public float jumpForce = 200;
+	void Start () {
+		
+	}
+	
+	void FixedUpdate () {
+		moveVector3.y -= gravity * Time.deltaTime;
+		if(charactercontroller.isGrounded){
+			if(Input.GetKey(KeyCode.Space)){
+			moveVector3.y += jumpForce * Time.deltaTime;
+			}
+		}
+		moveVector3.z = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+		charactercontroller.Move(moveVector3);
+	}
+}
