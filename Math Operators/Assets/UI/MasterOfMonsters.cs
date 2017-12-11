8bb1d63cdc268a4528d37d81f86cc0f03a84d3da
Unknown <mitchellwagner1997@gmail.com>
2017-12-11 15:13:04 -0700

    Finished Foreach loop

--- Math Operators/Assets/UI/MasterOfMonsters.cs
+++ Math Operators/Assets/UI/MasterOfMonsters.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class MasterOfMonsters : MonoBehaviour {
-public Text activeword;
+public GameObject text1;
+public GameObject text2;
+public GameObject text3;
+public int activationcode = 9;
 public string[] monsterstatement = {"Master", "of", "Monsters!!!"};
 	
 	void OnTriggerEnter()
 	{
+		activationcode -= 3;
+		//I know this is a really dumb use of a foreach loop, but
+		//it's the only thing I could come up with.
 		foreach (string value in monsterstatement)
 		{
-			print (monsterstatement);
+			if(activationcode <= 0){
+			text1.SetActive(true);
+			text2.SetActive(true);
+			text3.SetActive(true);
+			}
 		}
 	}
 }
