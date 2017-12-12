8c2d46b95f51295121dedf84e8f742f9d931d2d1
Unknown <mitchellwagner1997@gmail.com>
2017-12-12 11:45:04 -0700

    Started work on powerups coming back
    Powerups coming back on replay

--- Math Operators/Assets/UI/ReplayGame.cs
+++ Math Operators/Assets/UI/ReplayGame.cs
@@ -7,17 +7,20 @@ public class ReplayGame : MonoBehaviour {
 
 	public Transform player;
 	public Transform enemy;
+	public Transform Powerups;
 	public Image uiBar;
 	public GameObject GameOverUI;
 
 	public static Vector3 startPosition;
 	public static Vector3 enemyStart;
+	public static Vector3 powerupstart;
 	private float fillAmount;
 	void Awake()
 	{
 		startPosition = player.position;
 		enemyStart = enemy.position;
 		fillAmount = uiBar.fillAmount;
+		powerupstart = 
 		GameOverUI.SetActive(false);
 	}
 	public void Click () {
